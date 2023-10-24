9a3dc2fa5d010c399bf3f83a62dcbfd6d13ff9b5
Jason Tian <kuohuitien@gmail.com>
2023-10-24 12:34:50 +0800

    install Emacs 28 stable with snap

--- deploy/bootstrap.sh
+++ deploy/bootstrap.sh
@@ -9,8 +9,6 @@ sudo pacman -S --noconfirm git
 sudo pacman -S --noconfirm base-devel
 # https://wiki.archlinux.org/title/Arch_User_Repository
 
-
-
 # install snap
 git clone https://aur.archlinux.org/snapd.git
 cd snapd
@@ -19,16 +17,33 @@ makepkg -si --noconfirm --skippgpcheck
 sudo systemctl enable --now snapd.socket
 
 sudo ln -s /var/lib/snapd/snap /snap
-# either log out and back in again, or restart your system, to ensure snap's paths are updated correctly.
+
+# note: restart system to ensure snap's paths are updated correctly
 
 # install emacs28
 # pacman -S --noconfirm emacs
-
-# https://snapcraft.io/emacs
-
+sudo snap install emacs --channel=28.x/stable --classic
+# via https://snapcraft.io/emacs
 
 # clone my .emacs.d
 # cd ~
 # git clone https://github.com/jsntn/emacs.d .emacs.d || true
 
+# install python and pip
+# sudo pacman -S --noconfirm python
+
+# install pipx
+# sudo pacman -S --noconfirm python-pipx
+# pipx ensurepath
+
 # install pyright
+# pipx install pyright
+
+# install virtualenv
+# pipx install virtualenv
+
+# install zsh
+# sudo pacman -S --noconfirm zsh
+
+# install oh-my-zsh
+# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
