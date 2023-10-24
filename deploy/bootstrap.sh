51cae9d46cfb07829b97703162782f11bdf64d6c
Jason TIAN <jsntn@users.noreply.github.com>
2023-10-24 08:47:15 +0800

    update bootstrap script for snap and update Emacs installation

--- deploy/bootstrap.sh
+++ deploy/bootstrap.sh
@@ -9,11 +9,22 @@ sudo pacman -S --noconfirm git
 sudo pacman -S --noconfirm base-devel
 # https://wiki.archlinux.org/title/Arch_User_Repository
 
+
+
+# install snap
+git clone https://aur.archlinux.org/snapd.git
+cd snapd
+makepkg -si --noconfirm --skippgpcheck
+
+sudo systemctl enable --now snapd.socket
+
+sudo ln -s /var/lib/snapd/snap /snap
+# either log out and back in again, or restart your system, to ensure snap's paths are updated correctly.
+
 # install emacs28
 # pacman -S --noconfirm emacs
-git clone https://aur.archlinux.org/emacs28.git
-cd emacs28
-makepkg -si --noconfirm --skippgpcheck
+
+# https://snapcraft.io/emacs
 
 
 # clone my .emacs.d
